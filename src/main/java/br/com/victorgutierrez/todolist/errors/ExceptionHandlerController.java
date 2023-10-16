b954b12d9b8d4baab5d5720c98531276e50fceb9
VictorHugoGutierrez <hugovictorgutierrez@gmail.com>
2023-10-15 21:42:18 -0300

    upload files

---
+++ src/main/java/br/com/victorgutierrez/todolist/errors/ExceptionHandlerController.java
@@ -0,0 +1,17 @@
+package br.com.victorgutierrez.todolist.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMostSpecificCause().getMessage());
+  }
+
+}
